426811065aef99011a4a9c192bc5762503ea1270
Mohil koriya <mohil2005.k@gmail.com>
2025-07-16 22:23:38 +0530

    Arithmetic operation program

---
+++ arithmetic operations.py
@@ -0,0 +1,16 @@
+x = 2 + 3j 
+y = -1 + 2j
+
+# performing arithmetic operations
+
+sum = x+y
+diff = x-y
+multi = x*y
+div = x/y
+
+# Printing the result
+
+print("Sum:",sum)
+print("Differnce:",diff)
+print("Multiplication:",multi)
+print("Division:",div)
